837f9e3a32f0e4dd184e76d66c6f239ca16faca8
naavika <niksshahiruchi@gmail.com>
2024-03-21 16:41:48 +0530

    server requests

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     console.log(req);
+// });
+
+// server.listen(4000);
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const { url, method } = req;
+
+    if (url === '/home') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome home');
+    } else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to About Us page');
+    } else if (url === '/node') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to my Node Js project');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
 });
 
 server.listen(4000);
